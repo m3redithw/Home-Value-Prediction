8b9c40ab98055efc510266420a8997354b3e7e42
Meredith Wang <wang.meredith09@gmail.com>
2022-07-22 15:47:58 -0500

    added dummy variables

--- prepare.py
+++ prepare.py
@@ -15,20 +15,30 @@ def prep_zillow(df):
     
     # Change data type
     df['fips_code'] = df['fips_code'].astype(int)
-    df['year_built'] = df['year_built'].astype(int)
+    df['age'] = df['age'].astype(int)
     
     # Data mapping
     df['county'] = df.fips_code.map({6037: 'Los Angeles', 6059: 'Orange', 6111: 'Ventura'})
     
+    # Categorize bedrooms, bathrooms
+    df['bedrooms_size'] = pd.cut(df.bedrooms, bins = [0,2,4,6],
+                            labels = ['small', 'medium', 'large'])
+    df['bathrooms_size'] = pd.cut(df.bathrooms, bins = [0,2.5,4.5,6.5],
+                            labels = ['small', 'medium', 'large'])
+
     # Drop column
-    df = df.drop(['fips_code'], axis=1)
+    # df = df.drop(['fips_code'], axis=1)
+    
+    # Make dummy columns and concatenate on original dataframe 
+    dummy_df = pd.get_dummies(df[['county', 'bedrooms_size', 'bathrooms_size']], dummy_na=False, drop_first=False)
+    df = pd.concat([df, dummy_df], axis=1)
     
     # Handle Outliers:
     # The general rule for outliers are:
     ## Upper bond: Q3 + 1.5*IQR
     ## Lower bund: Q1 - 1.5*IQR
     # Bonds are manually adjusted for each feature
-    df = df[df.bedrooms <= 6.5]
+    df = df[df.bedrooms <= 6]
     df = df[df.bedrooms >= 1]
 
     df = df[df.bathrooms <= 6.5]
@@ -42,7 +52,7 @@ def prep_zillow(df):
 
     df = df[df.assessed_value <= 2520956]
     df = df[df.assessed_value >= 45366]
-
+    return df
 def split(df):
     '''
     This function drops the customer_id column and then splits a dataframe into 
